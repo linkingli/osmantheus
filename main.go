e855711288f905444553e71eb1359fd40e865de2
linkingli <852167929@qq.com>
2021-01-09 06:03:10 +0800

    [todo] add todo list

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	//todo args选择检测项
+
 	//非running的pod
 	petals.ListNotRunngPod()
 	//pod不满足调协的controller
@@ -12,4 +14,9 @@ func main() {
 	//测试节点之间的网络互通
 	petals.TestNodeConnect()
 
+	//todo 测试节点的io
+	//todo 测试节点的cpu
+	//todo 测试节点的缓存
+
+	//todo 测试所有节点的pv读写是否正常
 }
